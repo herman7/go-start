9e80bfc9b8102a4aea152f370f6a77ccb519bb09
herman365 <hermango365@gmail.com>
2024-11-22 17:30:49 +0800

    feat:interface

--- get-started/hello.go
+++ get-started/hello.go
@@ -2,8 +2,21 @@ package main
 
 import "fmt"
 
-import "rsc.io/quote"
-
 func main() {
-	fmt.Println(quote.Go())
+	// 声明
+	var pa, pb *int
+	// 取址
+	var a = 10 // 默认为 int
+	pa = &a
+	// 解引用（取值）
+	var b = *pa
+	// 输出
+	fmt.Println("a:", a)
+	fmt.Println("b:", b)
+	fmt.Println("&a:", &a)
+	fmt.Println("&b:", &b)
+	fmt.Println("pa:", pa)
+	fmt.Println("pb:", pb)
+	fmt.Println("*pa:", *pa)
+	// fmt.Println("*pb:", *pb)
 }
